82b1b9db901c43cbdc59e13d7eadc2142abd88a2
kvnwinata <kvnwinata@gmail.com>
2013-04-15 11:33:03 -0400

    new rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rocketscience::Application.config.secret_token = 'd5651cc06c6f8bc879219fb200af748192c6e971e0351d3824c38af5dda5d5b3e10a0499e1ba7828b66c5790e7a7c3fc5a4b11cd9de397cb039b2650fc781408'

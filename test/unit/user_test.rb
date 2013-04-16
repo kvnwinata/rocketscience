425a49be1abcf29b3aaa4b4d00492806301d550e
kvnwinata <kvnwinata@gmail.com>
2013-04-15 23:24:48 -0400

    user added

---
+++ test/unit/user_test.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                    :integer          not null, primary key
+#  email                 :string(255)
+#  password              :string(255)
+#  password_confirmation :string(255)
+#  password_digest       :string(255)
+#  created_at            :datetime         not null
+#  updated_at            :datetime         not null
+#
+
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end

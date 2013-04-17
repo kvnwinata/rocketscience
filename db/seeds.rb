7bc97d3199b4bd4643545bbff05d70fbce09f829
Ronnachai Jaroensri <tiam_12d@30-9-61.wireless.csail.mit.edu>
2013-04-17 15:26:58 -0400

    added images and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,9 +14,529 @@
 #  path        :string(255)
 #  category    :string(255)
 
+
 Image.create(
-	:id => 1,
-	:name => "image1",
-	:description => "image1",
-	:path => "image1",
-	:category => "category")
+    :id => 1,
+    :name => "tumblr_lxh1abULhm1qkfwf2o1_500.jpg",
+    :description => "American Traditional",
+    :path => "images/tattoos/",
+    :category => "American Traditional")
+
+Image.create(
+    :id => 2,
+    :name => "american-traditional-tattoo-flash-5158c6ed-sz450x300-animate.jpg",
+    :description => "American Traditional",
+    :path => "images/tattoos/",
+    :category => "American Traditional")
+
+Image.create(
+    :id => 3,
+    :name => "AmericanTraditionalWeb-Ready.jpg",
+    :description => "American Traditional",
+    :path => "images/tattoos/",
+    :category => "American Traditional")
+
+Image.create(
+    :id => 4,
+    :name => "2381513772_cf97eb6ec7.jpg",
+    :description => "American Traditional",
+    :path => "images/tattoos/",
+    :category => "American Traditional")
+
+Image.create(
+    :id => 5,
+    :name => "tumblr_mfezmiAyCQ1qceyj5o1_500.jpg",
+    :description => "American Traditional",
+    :path => "images/tattoos/",
+    :category => "American Traditional")
+
+Image.create(
+    :id => 6,
+    :name => "tottoos.jpg",
+    :description => "American Traditional",
+    :path => "images/tattoos/",
+    :category => "American Traditional")
+
+Image.create(
+    :id => 7,
+    :name => "traditional-tattoo-eagle-murray-sell.jpg",
+    :description => "American Traditional",
+    :path => "images/tattoos/",
+    :category => "American Traditional")
+
+Image.create(
+    :id => 8,
+    :name => "chest-piece-tattoo-m.jpg",
+    :description => "American Traditional",
+    :path => "images/tattoos/",
+    :category => "American Traditional")
+
+Image.create(
+    :id => 9,
+    :name => "tumblr_macs9rU5Em1qfp499o1_500.jpg",
+    :description => "American Traditional",
+    :path => "images/tattoos/",
+    :category => "American Traditional")
+
+Image.create(
+    :id => 10,
+    :name => "420409_10151199209227159_813968611_n.jpg",
+    :description => "American Traditional",
+    :path => "images/tattoos/",
+    :category => "American Traditional")
+
+Image.create(
+    :id => 11,
+    :name => "001_06.JPG",
+    :description => "American Traditional",
+    :path => "images/tattoos/",
+    :category => "American Traditional")
+
+Image.create(
+    :id => 12,
+    :name => "Rk66X.jpg",
+    :description => "American Traditional",
+    :path => "images/tattoos/",
+    :category => "American Traditional")
+
+Image.create(
+    :id => 13,
+    :name => "25684660345257793NXMjEF54c.jpg",
+    :description => "American Traditional",
+    :path => "images/tattoos/",
+    :category => "American Traditional")
+
+Image.create(
+    :id => 14,
+    :name => "tumblr_me4c5c2Qob1rjli03o1_500.jpg",
+    :description => "American Traditional",
+    :path => "images/tattoos/",
+    :category => "American Traditional")
+
+Image.create(
+    :id => 15,
+    :name => "tumblr_l68a4kXB5Q1qcmz1jo1_500.jpg",
+    :description => "American Traditional",
+    :path => "images/tattoos/",
+    :category => "American Traditional")
+
+Image.create(
+    :id => 16,
+    :name => "0896597989interior01.jpg",
+    :description => "Japanese Traditional",
+    :path => "images/tattoos/",
+    :category => "Japanese Traditional")
+
+Image.create(
+    :id => 17,
+    :name => "Cherry-blossom-Color-Japanese-Tattoo.jpg",
+    :description => "Japanese Traditional",
+    :path => "images/tattoos/",
+    :category => "Japanese Traditional")
+
+Image.create(
+    :id => 18,
+    :name => "images-1.jpeg",
+    :description => "Japanese Traditional",
+    :path => "images/tattoos/",
+    :category => "Japanese Traditional")
+
+Image.create(
+    :id => 19,
+    :name => "images.jpeg",
+    :description => "Japanese Traditional",
+    :path => "images/tattoos/",
+    :category => "Japanese Traditional")
+
+Image.create(
+    :id => 20,
+    :name => "japanese_tattoo_flower_peony.jpg",
+    :description => "Japanese Traditional",
+    :path => "images/tattoos/",
+    :category => "Japanese Traditional")
+
+Image.create(
+    :id => 21,
+    :name => "japanese-tattoo-01.jpg",
+    :description => "Japanese Traditional",
+    :path => "images/tattoos/",
+    :category => "Japanese Traditional")
+
+Image.create(
+    :id => 22,
+    :name => "japanese-tattoo-gallery2.jpg",
+    :description => "Japanese Traditional",
+    :path => "images/tattoos/",
+    :category => "Japanese Traditional")
+
+Image.create(
+    :id => 23,
+    :name => "japanese042.jpg",
+    :description => "Japanese Traditional",
+    :path => "images/tattoos/",
+    :category => "Japanese Traditional")
+
+Image.create(
+    :id => 24,
+    :name => "t1_Japanese-Tattoos-samurai_705.jpg",
+    :description => "Japanese Traditional",
+    :path => "images/tattoos/",
+    :category => "Japanese Traditional")
+
+Image.create(
+    :id => 25,
+    :name => "t1_Japanese-Tattoos-samurai_755.jpg",
+    :description => "Japanese Traditional",
+    :path => "images/tattoos/",
+    :category => "Japanese Traditional")
+
+Image.create(
+    :id => 26,
+    :name => "This-cherry-blossom-tree-tattoo-design-looks-like-a-traditional-Japanese-watercolor-painting.-250x375.jpg",
+    :description => "Japanese Traditional",
+    :path => "images/tattoos/",
+    :category => "Japanese Traditional")
+
+Image.create(
+    :id => 27,
+    :name => "traditional_japanese_tattoo_horisyshiIII.jpg",
+    :description => "Japanese Traditional",
+    :path => "images/tattoos/",
+    :category => "Japanese Traditional")
+
+Image.create(
+    :id => 28,
+    :name => "Traditional-Japanese-tattoos-Wabori-1.jpg",
+    :description => "Japanese Traditional",
+    :path => "images/tattoos/",
+    :category => "Japanese Traditional")
+
+Image.create(
+    :id => 29,
+    :name => "tumblr_liul34YXZw1qb0yjt.jpg",
+    :description => "Japanese Traditional",
+    :path => "images/tattoos/",
+    :category => "Japanese Traditional")
+
+Image.create(
+    :id => 30,
+    :name => "tumblr_lzq8jfSOnT1qgs3yho1_500.jpg",
+    :description => "Japanese Traditional",
+    :path => "images/tattoos/",
+    :category => "Japanese Traditional")
+
+Image.create(
+    :id => 31,
+    :name => "Profile_captain.jpg",
+    :description => "Horror and Gore",
+    :path => "images/tattoos/",
+    :category => "Horror and Gore")
+
+Image.create(
+    :id => 32,
+    :name => "l_bd670f0dbb6e3e0a33a6ce913b04af93.jpg",
+    :description => "Horror and Gore",
+    :path => "images/tattoos/",
+    :category => "Horror and Gore")
+
+Image.create(
+    :id => 33,
+    :name => "l.jpg",
+    :description => "Horror and Gore",
+    :path => "images/tattoos/",
+    :category => "Horror and Gore")
+
+Image.create(
+    :id => 34,
+    :name => "edclown.jpg",
+    :description => "Horror and Gore",
+    :path => "images/tattoos/",
+    :category => "Horror and Gore")
+
+Image.create(
+    :id => 35,
+    :name => "gore-print-Dan-Henk.jpg",
+    :description => "Horror and Gore",
+    :path => "images/tattoos/",
+    :category => "Horror and Gore")
+
+Image.create(
+    :id => 36,
+    :name => "tumblr_mats8vRqqq1rhwqp7o1_500.jpg",
+    :description => "Horror and Gore",
+    :path => "images/tattoos/",
+    :category => "Horror and Gore")
+
+Image.create(
+    :id => 37,
+    :name => "leather-tattoo.jpg",
+    :description => "Horror and Gore",
+    :path => "images/tattoos/",
+    :category => "Horror and Gore")
+
+Image.create(
+    :id => 38,
+    :name => "Self_Tattooed_Hand_by_KatGore.jpg",
+    :description => "Horror and Gore",
+    :path => "images/tattoos/",
+    :category => "Horror and Gore")
+
+Image.create(
+    :id => 39,
+    :name => "406-31011l.jpg",
+    :description => "Horror and Gore",
+    :path => "images/tattoos/",
+    :category => "Horror and Gore")
+
+Image.create(
+    :id => 40,
+    :name => "zombie.jpg",
+    :description => "Horror and Gore",
+    :path => "images/tattoos/",
+    :category => "Horror and Gore")
+
+Image.create(
+    :id => 41,
+    :name => "1001_lrap_12_o+mexico_city_tattoo_convention+monster.jpg",
+    :description => "Horror and Gore",
+    :path => "images/tattoos/",
+    :category => "Horror and Gore")
+
+Image.create(
+    :id => 42,
+    :name => "petestrain.jpg",
+    :description => "Horror and Gore",
+    :path => "images/tattoos/",
+    :category => "Horror and Gore")
+
+Image.create(
+    :id => 43,
+    :name => "56_d861b2f4c192bb9463bba5302011eea11332274919.jpg",
+    :description => "Horror and Gore",
+    :path => "images/tattoos/",
+    :category => "Horror and Gore")
+
+Image.create(
+    :id => 44,
+    :name => "ashleythigh.jpg",
+    :description => "Horror and Gore",
+    :path => "images/tattoos/",
+    :category => "Horror and Gore")
+
+Image.create(
+    :id => 45,
+    :name => "zombie-tattoo.jpg",
+    :description => "Horror and Gore",
+    :path => "images/tattoos/",
+    :category => "Horror and Gore")
+
+Image.create(
+    :id => 46,
+    :name => "5017468544_b23b016d6e_m.jpg",
+    :description => "Black and Grey",
+    :path => "images/tattoos/",
+    :category => "Black and Grey")
+
+Image.create(
+    :id => 47,
+    :name => "HotChick_Black_Grey-Tattoos16.jpg",
+    :description => "Black and Grey",
+    :path => "images/tattoos/",
+    :category => "Black and Grey")
+
+Image.create(
+    :id => 48,
+    :name => "2798753295_b66b809701.jpg",
+    :description => "Black and Grey",
+    :path => "images/tattoos/",
+    :category => "Black and Grey")
+
+Image.create(
+    :id => 49,
+    :name => "blacktattoo29.jpg",
+    :description => "Black and Grey",
+    :path => "images/tattoos/",
+    :category => "Black and Grey")
+
+Image.create(
+    :id => 50,
+    :name => "st-michael-tattoo.jpg",
+    :description => "Black and Grey",
+    :path => "images/tattoos/",
+    :category => "Black and Grey")
+
+Image.create(
+    :id => 51,
+    :name => "black-grey-tattoo.jpg",
+    :description => "Black and Grey",
+    :path => "images/tattoos/",
+    :category => "Black and Grey")
+
+Image.create(
+    :id => 52,
+    :name => "angel_tattoo_black_and_grey_by_Remistattoo.jpg",
+    :description => "Black and Grey",
+    :path => "images/tattoos/",
+    :category => "Black and Grey")
+
+Image.create(
+    :id => 53,
+    :name => "black-grey-tattoo-06-curatedmag-360x540.jpg",
+    :description => "Black and Grey",
+    :path => "images/tattoos/",
+    :category => "Black and Grey")
+
+Image.create(
+    :id => 54,
+    :name => "black_and_grey_vol1_12_1.jpg",
+    :description => "Black and Grey",
+    :path => "images/tattoos/",
+    :category => "Black and Grey")
+
+Image.create(
+    :id => 55,
+    :name => "boegarretts.jpg",
+    :description => "Black and Grey",
+    :path => "images/tattoos/",
+    :category => "Black and Grey")
+
+Image.create(
+    :id => 56,
+    :name => "black and grey calla lily foot tattoo.jpg",
+    :description => "Black and Grey",
+    :path => "images/tattoos/",
+    :category => "Black and Grey")
+
+Image.create(
+    :id => 57,
+    :name => "Grey_tattoo_144.jpg",
+    :description => "Black and Grey",
+    :path => "images/tattoos/",
+    :category => "Black and Grey")
+
+Image.create(
+    :id => 58,
+    :name => "black-and-grey-tattoos-by-eric-marcinizyn-a.jpg",
+    :description => "Black and Grey",
+    :path => "images/tattoos/",
+    :category => "Black and Grey")
+
+Image.create(
+    :id => 59,
+    :name => "l (1).jpg",
+    :description => "Black and Grey",
+    :path => "images/tattoos/",
+    :category => "Black and Grey")
+
+Image.create(
+    :id => 60,
+    :name => "black-and-grey-tattoos-by-eric-marcinizyn-2.jpg",
+    :description => "Black and Grey",
+    :path => "images/tattoos/",
+    :category => "Black and Grey")
+
+Image.create(
+    :id => 61,
+    :name => "illustrated-swamp.jpg",
+    :description => "Nature Inspired",
+    :path => "images/tattoos/",
+    :category => "Nature Inspired")
+
+Image.create(
+    :id => 62,
+    :name => "0916-temptu-tattoo-my-arm_bd.jpg",
+    :description => "Nature Inspired",
+    :path => "images/tattoos/",
+    :category => "Nature Inspired")
+
+Image.create(
+    :id => 63,
+    :name => "bird tattoonow large.jpg",
+    :description => "Nature Inspired",
+    :path => "images/tattoos/",
+    :category => "Nature Inspired")
+
+Image.create(
+    :id => 64,
+    :name => "1316399631_c612823714_b.jpg",
+    :description => "Nature Inspired",
+    :path => "images/tattoos/",
+    :category => "Nature Inspired")
+
+Image.create(
+    :id => 65,
+    :name => "1a3a59c434e9641224b766c69c805ee6.jpg",
+    :description => "Nature Inspired",
+    :path => "images/tattoos/",
+    :category => "Nature Inspired")
+
+Image.create(
+    :id => 66,
+    :name => "stencil kitty tattoo now2.jpg",
+    :description => "Nature Inspired",
+    :path => "images/tattoos/",
+    :category => "Nature Inspired")
+
+Image.create(
+    :id => 67,
+    :name => "a-hummingbird-butterfly-and-flowers-make-up-this-beautiful-tattoo-a-perfect-tattoo-design-for-women-or-girls.jpg",
+    :description => "Nature Inspired",
+    :path => "images/tattoos/",
+    :category => "Nature Inspired")
+
+Image.create(
+    :id => 68,
+    :name => "a-paisley-feather-tattoo-design-that-symbolizes-spirituality-594x445.jpg",
+    :description => "Nature Inspired",
+    :path => "images/tattoos/",
+    :category => "Nature Inspired")
+
+Image.create(
+    :id => 69,
+    :name => "a-peacock-feather-tattoo-on-the-leg-showing-off-the-eye-of-the-peacock-feather1.jpg",
+    :description => "Nature Inspired",
+    :path => "images/tattoos/",
+    :category => "Nature Inspired")
+
+Image.create(
+    :id => 70,
+    :name => "Tree-Tattoo-Design-For-Sexy-Girl-Rib-Cage.jpg",
+    :description => "Nature Inspired",
+    :path => "images/tattoos/",
+    :category => "Nature Inspired")
+
+Image.create(
+    :id => 71,
+    :name => "tree-tattoo.jpg",
+    :description => "Nature Inspired",
+    :path => "images/tattoos/",
+    :category => "Nature Inspired")
+
+Image.create(
+    :id => 72,
+    :name => "free-bird-tattoo.jpg",
+    :description => "Nature Inspired",
+    :path => "images/tattoos/",
+    :category => "Nature Inspired")
+
+Image.create(
+    :id => 73,
+    :name => "bird-5.jpg",
+    :description => "Nature Inspired",
+    :path => "images/tattoos/",
+    :category => "Nature Inspired")
+
+Image.create(
+    :id => 74,
+    :name => "mountains_tattoo2.jpg",
+    :description => "Nature Inspired",
+    :path => "images/tattoos/",
+    :category => "Nature Inspired")
+
+Image.create(
+    :id => 75,
+    :name => "tumblr_mdgrp8Troc1r07v7lo1_500.jpg",
+    :description => "Nature Inspired",
+    :path => "images/tattoos/",
+    :category => "Nature Inspired")
+

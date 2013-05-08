a18bbdf4c674a8bfb268d3ccec437c3f5b1346b2
kvnwinata <kvnwinata@gmail.com>
2013-05-08 04:17:10 -0400

    artist filter working

--- app/assets/javascripts/artist-filter.js
+++ app/assets/javascripts/artist-filter.js
@@ -2,20 +2,18 @@
 var render_artist_tab = function(data,status){
 	$("#infoPanel").html(""); // clear content
 	for(var id in data){
-			if (data.hasOwnProperty(id)){
-				var tab = document.createElement('div');
-				$(tab).html('<span class="artist-name">'+id+'</span>
-    <span class="artist-email">('+data[id].email+')</span><br>
-    <span class="artist-address">'+data[id].addr1+'</span><br>
-    <span class="artist-email">'+data[id].addr1+'</span>').appendTo($("#infoPanel"));
-
-			
+		if (data.hasOwnProperty(id)){
+			var tab = document.createElement('div');
+			$(tab).addClass("artist-tab inactiveTab");
+			$(tab).attr('id', id);
+			$(tab).html('<span class="artist-name">'+data[id].name+'</span><span class="artist-email">('+data[id].email+')</span><br><span class="artist-address">'+data[id].address1+'</span><br><span class="artist-email">'+data[id].address2+'</span>').appendTo($("#infoPanel"));
 		}
 		tab_setup();
-}
+	}
+};
 var pull_artist = function(category){
 	$.ajax({
-		url: "/images/get_category",
+		url: "/artists/get_category",
 		type: "GET",
 		headers: {
 			'X-CSRF-Token':$('meta[name="csrf-token"]').attr('content')
@@ -26,4 +24,4 @@ var pull_artist = function(category){
 		},
 		success: render_artist_tab
 	});
-}
+};

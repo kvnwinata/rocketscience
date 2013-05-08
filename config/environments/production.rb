7b4d847a10570146f0db561f9494cd7235581b8c
kvnwinata <kvnwinata@gmail.com>
2013-05-08 08:45:18 -0400

    asdasd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,5 +35,5 @@ Rocketscience::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-  #config.assets.compile = true
+  config.assets.compile = true
 end
